2baa5d2cbc0ccaf41e06ed0ce61d59016d96970a
Matt <matthewsimonmould@gmail.com>
2016-05-02 11:16:20 +0100

    Initial Commit

---
+++ autolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayout
+//
+//  Created by Matthew Mould on 02/05/2016.
+//  Copyright © 2016 Matthew Mould. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayout
+//
+//  Created by Matthew Mould on 02/05/2016.
+//  Copyright © 2016 Matthew Mould. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
